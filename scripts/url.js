1c726aba6cda8945e84a80f40189c7b8b5584db2
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-02-16 20:16:46 -0300

    add optional params in url & create default dates

--- scripts/url.js
+++ scripts/url.js
@@ -1,18 +1,24 @@
-function generateUrl(destiny = 'Buenos Aires', checkin = getDefoultCheckinDate(), checkout = getDefoultCheckoutDate()) {
+function generateUrl(destiny, checkin, checkout, count) {
     // TODO: modify the offset to change pages..
-    const url = `https://www.booking.com/searchresults.es-ar.html?label=gen173nr-1FCAQoggJCFHNlYXJjaF9tYXIgZGVsIHBsYXRhSCxYBGgMiAEBmAEsuAEXyAEM2AEB6AEB-AEDiAIBqAIDuALrt-6cBsACAdICJGVmMTFmOTg2LTFlMTQtNDY4OS04OGE5LWMyMDcwMmMyOTEzYdgCBeACAQ&aid=304142&lang=es-ar&ss=${destiny}&sb_travel_purpose=leisure&group_adults=2&sb=1&checkout=${checkout}&checkin=${checkin}&group_children=0&no_rooms=1&src=searchresults&src_elem=sb&offset=0`;
+    (!checkin || checkin == undefined || checkin === null || checkin === '') ? checkin = getDefoultCheckinDate() : checkin;
+    (!checkout || checkout == undefined || checkout === null || checkout === '') ? checkout = getDefoultCheckoutDate() : checkout;
+    (!destiny || destiny == undefined || destiny === null || destiny === '') ? destiny = 'Buenos+Aires' : destiny;
+    (!count || count == undefined || count === null || count === '') ? count = 0 : count;
+
+    const url = `https://www.booking.com/searchresults.es-ar.html?label=gen173nr-1FCAQoggJCFHNlYXJjaF9tYXIgZGVsIHBsYXRhSCxYBGgMiAEBmAEsuAEXyAEM2AEB6AEB-AEDiAIBqAIDuALrt-6cBsACAdICJGVmMTFmOTg2LTFlMTQtNDY4OS04OGE5LWMyMDcwMmMyOTEzYdgCBeACAQ&aid=304142&lang=es-ar&ss=${destiny}&sb_travel_purpose=leisure&group_adults=2&sb=1&checkout=${checkout}&checkin=${checkin}&group_children=0&no_rooms=1&src=searchresults&src_elem=sb&offset=${count}`;
 
     return url;
 }
 
-// TODO: defoult-date-functions are incompletes. Need validations (next month (+1) when day > 30 || day > 31. etc).
-// TODO: need to check validation of days and months when they change. 
 
 function getDefoultCheckinDate() {
-    const date = new Date();
 
-    const day = date.getDate() + 1;
-    const month = date.getMonth() + 1;
+    const date = new Date();
+    date.setDate(date.getDate() + 15);
+
+
+    const day = ('0' + date.getDate()).slice(-2)//fecha exacta de hoy; 
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);//retorna un mes menos (start from 0)
     const year = date.getFullYear();
 
     return `${year}-${month}-${day}`
@@ -20,9 +26,10 @@ function getDefoultCheckinDate() {
 
 function getDefoultCheckoutDate() {
     const date = new Date();
+    date.setDate(date.getDate() + 20);
 
-    const day = date.getDate() + 7;
-    const month = date.getMonth() + 1;
+    const day = ('0' + date.getDate()).slice(-2)
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const year = date.getFullYear();
 
     return `${year}-${month}-${day}`
