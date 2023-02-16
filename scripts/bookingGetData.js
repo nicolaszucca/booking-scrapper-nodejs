9fd0a3687f0090dac9147b94a5cc7a8c04b9e662
Nicolas Zuccarello <zuccarelo1@gmail.com>
2023-02-16 20:21:22 -0300

    Change generateUrl path

--- scripts/bookingGetData.js
+++ scripts/bookingGetData.js
@@ -4,38 +4,51 @@ const { generateUrl } = require("./url");
 
 class BookingData {
     //Inicializa el navegador de puppeteer
-    async initBrowser() {
+    async initBrowser(destiny, checkin, checkout) {
         this.browser = await puppeteer.launch({ headless: false });
         this.page = await this.browser.newPage();
+        this.pageUrl = generateUrl(destiny, checkin, checkout);
     };
 
     //Obtiene los links de los apartamentos
-    async getApartamentLinks(destiny, checkin, checkout) {
-        const pageUrl = generateUrl(destiny, checkin, checkout);
-        await this.page.goto(pageUrl);
-        const housePreview = new HousePreviewParser();
+    async getApartamentLinks() {
 
-        const apartmentsElements = await this.page.$$(".da89aeb942");
+        try {
+            await this.page.goto(this.pageUrl);
 
-        let links = [];
-        for (let apartamentElement of apartmentsElements) {
-            const houseData = await housePreview.getLink(apartamentElement);
-            const link = houseData.link;
-            links.push(link);
+            const housePreview = new HousePreviewParser();
+
+            const apartmentsElements = await this.page.$$(".da89aeb942");
+
+            let links = [];
+            for (let apartamentElement of apartmentsElements) {
+                const houseData = await housePreview.getLink(apartamentElement);
+                const link = houseData.link;
+                links.push(link);
+            }
+            return links;
+            //REFERENCE: previous function in evaluate method
+            //function getLink(items) {
+            //    return items.querySelector('div.c90a25d457 > a').href;
+            //} 
+
+        } catch (error) {
+            throw new Error('Cannot access', error);
         }
-        return links;
-        //REFERENCE: previous function in evaluate method 
-        //function getLink(items) {
-        //    return items.querySelector('div.c90a25d457 > a').href;
-        //} 
     };
 
     async parserDepartamentData(url) {
-        await this.page.goto(url);
-        const housePage = new HousePageParser();
-        const data = await housePage.parserDepartamentPage(this.page);
+        try {
+            await this.page.goto(url);
+            const housePage = new HousePageParser();
+            const data = await housePage.parserDepartamentPage(this.page);
 
-        return data;
+            return data;
+
+        } catch (error) {
+            throw new Error('Cannot access', error);
+        }
+
     };
 
     async stopBrowser() {
